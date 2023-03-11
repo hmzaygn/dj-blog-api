68670783e43d842ac8fb78cc7ad5a3e7693c6b90
Feanor <hamzaaygan1995@gmail.com>
2023-03-11 17:33:15 +0300

    minor fixes

--- users/serializers.py
+++ users/serializers.py
@@ -28,12 +28,14 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
-        'username',
-        'first_name',
-        'last_name',
-        'email',
-        'password',
-        'password2']
+            "id",
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'password2'
+            ]
 
     extra_kwargs = {
             "password": {"write_only": True},
